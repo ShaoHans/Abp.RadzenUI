ae6391f592cf43a0f29469270a4dbad997a26811
Hans <shaomenghao@hotmail.com>
2024-10-09 18:49:11 +0800

    add fonts css

--- src/Abp.RadzenUI.Blazor.Server/Bundling/BlazorGlobalStyleContributor.cs
+++ src/Abp.RadzenUI.Blazor.Server/Bundling/BlazorGlobalStyleContributor.cs
@@ -7,14 +7,6 @@ public class BlazorGlobalStyleContributor : BundleContributor
     public override void ConfigureBundle(BundleConfigurationContext context)
     {
         context.Files.AddIfNotContains("/_content/Abp.RadzenUI.Blazor.Server/bootstrap/bootstrap.min.css");
-        context.Files.AddIfNotContains("/_content/Abp.RadzenUI.Blazor.Server/css/fluent-base.css");
-        context.Files.AddIfNotContains("/_content/Abp.RadzenUI.Blazor.Server/css/fluent-dark-base.css");
-        context.Files.AddIfNotContains("/_content/Abp.RadzenUI.Blazor.Server/css/fluent-dark-wcag.css");
-        context.Files.AddIfNotContains("/_content/Abp.RadzenUI.Blazor.Server/css/fluent-wcag.css");
-        context.Files.AddIfNotContains("/_content/Abp.RadzenUI.Blazor.Server/css/material3-base.css");
-        context.Files.AddIfNotContains("/_content/Abp.RadzenUI.Blazor.Server/css/material3-dark-base.css");
-        context.Files.AddIfNotContains("/_content/Abp.RadzenUI.Blazor.Server/css/material3-dark-wcag.css");
-        context.Files.AddIfNotContains("/_content/Abp.RadzenUI.Blazor.Server/css/material3-wcag.css");
         context.Files.AddIfNotContains("/_content/Abp.RadzenUI.Blazor.Server/css/site.css");
         context.Files.AddIfNotContains("/_content/Abp.RadzenUI.Blazor.Server/app.css");
 
