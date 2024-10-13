3df46371dd2bf5dc8349eca050c6ac1914cc13a6
shaohanzhong <shaomenghao@hotmail.com>
2024-10-13 22:02:16 +0800

    add TitleBarOptions

--- src/Abp.RadzenUI.Blazor.Server/AbpRadzenUIOptions.cs
+++ src/Abp.RadzenUI.Blazor.Server/AbpRadzenUIOptions.cs
@@ -6,6 +6,16 @@ public class AbpRadzenUIOptions
 {
     public List<Assembly> RouterAdditionalAssemblies { get; set; } = [];
 
+    public TitleBarOptions TitleBar { get; set; } = new();
+}
+
+public class TitleBarOptions
+{
+    /// <summary>
+    /// title bar header name
+    /// </summary>
+    public string Title { get; set; } = "Abp RadzenUI";
+
     /// <summary>
     /// Whether to display the GitHub address of the project in the title bar
     /// </summary>
