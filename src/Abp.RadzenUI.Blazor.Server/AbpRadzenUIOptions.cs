ad2ef64d0cdea24b1f1ffee5b0e3e0e23e74c952
shaohanzhong <shaomenghao@hotmail.com>
2024-10-13 20:02:02 +0800

    title bar add github link

--- src/Abp.RadzenUI.Blazor.Server/AbpRadzenUIOptions.cs
+++ src/Abp.RadzenUI.Blazor.Server/AbpRadzenUIOptions.cs
@@ -5,4 +5,9 @@ namespace Abp.RadzenUI;
 public class AbpRadzenUIOptions
 {
     public List<Assembly> RouterAdditionalAssemblies { get; set; } = [];
+
+    /// <summary>
+    /// Whether to display the GitHub address of the project in the title bar
+    /// </summary>
+    public bool ShowGithubLink = true;
 }
