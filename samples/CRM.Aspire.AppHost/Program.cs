b7eefdf4579ded2c30ff6c61750225dd281458ea
shaohanzhong <shaomenghao@hotmail.com>
2024-10-23 22:45:44 +0800

    update AppHost project

--- samples/CRM.Aspire.AppHost/Program.cs
+++ samples/CRM.Aspire.AppHost/Program.cs
@@ -1,5 +1,19 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.CRM_Blazor_Web>("crm-blazor-web");
+var redis = builder.AddRedis("redis", 6379).WithImageTag("7.2");
+var username = builder.AddParameter("pguser");
+var password = builder.AddParameter("pgpwd");
+var db = builder.AddPostgres("postgres", username, password, 5432).WithImageTag("16.2");
+
+var migrator = builder
+    .AddProject<Projects.CRM_DbMigrator>("crm-db-migrator")
+    .WithReference(redis)
+    .WithReference(db);
+
+builder
+    .AddProject<Projects.CRM_Blazor_Web>("crm-blazor-web")
+    .WithReference(migrator)
+    .WithReference(redis)
+    .WithReference(db);
 
 builder.Build().Run();
