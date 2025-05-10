d62ea1df5898b3e04d1eacaadbe13153f224437d
ShaoHans <shaomenghao@hotmail.com>
2025-05-10 11:52:29 +0800

    modify LoginLayout ,the Login page use LoginLayout

--- src/Abp.Blazor.Server.RadzenUI/Components/Pages/Account/Login.razor.cs
+++ src/Abp.Blazor.Server.RadzenUI/Components/Pages/Account/Login.razor.cs
@@ -33,31 +33,31 @@ public partial class Login
         }
     }
 
-    async Task OpenTenantSwitchDialog()
-    {
-        await DialogService.OpenAsync<TenantSwitchDialog>(
-            title: TL["SwitchTenant"],
-            parameters: new Dictionary<string, object>
-            {
-                { "TenantName", CurrentTenant?.Name ?? string.Empty }
-            },
-            options: new DialogOptions
-            {
-                Draggable = true,
-                Width = "550px",
-                Height = "400px"
-            }
-        );
-    }
+    //async Task OpenTenantSwitchDialog()
+    //{
+    //    await DialogService.OpenAsync<TenantSwitchDialog>(
+    //        title: TL["SwitchTenant"],
+    //        parameters: new Dictionary<string, object>
+    //        {
+    //            { "TenantName", CurrentTenant?.Name ?? string.Empty }
+    //        },
+    //        options: new DialogOptions
+    //        {
+    //            Draggable = true,
+    //            Width = "550px",
+    //            Height = "400px"
+    //        }
+    //    );
+    //}
 
-    private void LoginAzure()
-    {
-        // 跳转到 ABP 的外部登录地址，provider=AzureOpenId 对应上面配置的方案名
-        var returnUrl = Navigation.Uri; // 登录后返回当前页面
-        var encoded = Uri.EscapeDataString(returnUrl);
-        Navigation.NavigateTo(
-            $"Account/ExternalLogin?provider=AzureOpenId&returnUrl={encoded}",
-            forceLoad: true
-        );
-    }
+    //private void LoginAzure()
+    //{
+    //    // 跳转到 ABP 的外部登录地址，provider=AzureOpenId 对应上面配置的方案名
+    //    var returnUrl = Navigation.Uri; // 登录后返回当前页面
+    //    var encoded = Uri.EscapeDataString(returnUrl);
+    //    Navigation.NavigateTo(
+    //        $"Account/ExternalLogin?provider=AzureOpenId&returnUrl={encoded}",
+    //        forceLoad: true
+    //    );
+    //}
 }
