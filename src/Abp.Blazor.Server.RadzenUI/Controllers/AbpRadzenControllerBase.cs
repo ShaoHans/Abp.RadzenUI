75d439693b76ae8ea90e856b8a4d85236e241bcc
ShaoHans <shaomenghao@hotmail.com>
2025-05-10 16:07:15 +0800

    external login and callback

--- src/Abp.Blazor.Server.RadzenUI/Controllers/AbpRadzenControllerBase.cs
+++ src/Abp.Blazor.Server.RadzenUI/Controllers/AbpRadzenControllerBase.cs
@@ -2,15 +2,23 @@
 using Microsoft.Extensions.Logging;
 using Volo.Abp;
 using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.UI.Navigation.Urls;
 using Volo.Abp.Validation;
 
 namespace Abp.RadzenUI.Controllers;
 
 public class AbpRadzenControllerBase : AbpControllerBase
 {
+    protected IAppUrlProvider AppUrlProvider => LazyServiceProvider.LazyGetRequiredService<IAppUrlProvider>();
+
+    protected virtual IActionResult RedirectWithError(string url, string error)
+    {
+        return Redirect($"{url}?error={error}");
+    }
+
     protected virtual IActionResult RedirectWithError(string url, Exception ex)
     {
-        return Redirect($"{url}?error=" + GetErrorMessage(ex));
+        return Redirect($"{url}?error={GetErrorMessage(ex)}");
     }
 
     protected virtual string GetErrorMessage(Exception ex)
@@ -28,5 +36,42 @@ public class AbpRadzenControllerBase : AbpControllerBase
         }
 
         return error;
+    }
+
+    protected virtual async Task<RedirectResult> RedirectSafelyAsync(string returnUrl, string? returnUrlHash = null)
+    {
+        return Redirect(await GetRedirectUrlAsync(returnUrl, returnUrlHash));
+    }
+
+    protected virtual async Task<string> GetRedirectUrlAsync(string returnUrl, string? returnUrlHash = null)
+    {
+        returnUrl = await NormalizeReturnUrlAsync(returnUrl);
+
+        if (!returnUrlHash.IsNullOrWhiteSpace())
+        {
+            returnUrl += returnUrlHash;
+        }
+
+        return returnUrl;
+    }
+
+    protected virtual async Task<string> NormalizeReturnUrlAsync(string returnUrl)
+    {
+        if (returnUrl.IsNullOrEmpty())
+        {
+            return await GetAppHomeUrlAsync();
+        }
+
+        if (Url.IsLocalUrl(returnUrl) || await AppUrlProvider.IsRedirectAllowedUrlAsync(returnUrl))
+        {
+            return returnUrl;
+        }
+
+        return await GetAppHomeUrlAsync();
+    }
+
+    protected virtual Task<string> GetAppHomeUrlAsync()
+    {
+        return Task.FromResult("~/"); //TODO: ???
     }
 }
--- src/Abp.Blazor.Server.RadzenUI/Controllers/AccountController.cs
+++ src/Abp.Blazor.Server.RadzenUI/Controllers/AccountController.cs
@@ -1,10 +1,15 @@
-﻿using Microsoft.AspNetCore.Authentication;
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Volo.Abp;
 using Volo.Abp.Account;
 using Volo.Abp.Auditing;
 using Volo.Abp.Identity;
 using Volo.Abp.Identity.AspNetCore;
+using Volo.Abp.Security.Claims;
 
 namespace Abp.RadzenUI.Controllers;
 
@@ -13,7 +18,8 @@ public class AccountController(
     IdentitySecurityLogManager identitySecurityLogManager,
     IAccountAppService accountAppService,
     IdentityUserManager userManager,
-    IdentityDynamicClaimsPrincipalContributorCache identityDynamicClaimsPrincipalContributorCache
+    IdentityDynamicClaimsPrincipalContributorCache identityDynamicClaimsPrincipalContributorCache,
+    IOptions<IdentityOptions> identityOptions
 ) : AbpRadzenControllerBase
 {
     [HttpPost("/account/login")]
@@ -22,7 +28,12 @@ public class AccountController(
     {
         try
         {
-            var result = await signInManager.PasswordSignInAsync(username, password, rememberMe, true);
+            var result = await signInManager.PasswordSignInAsync(
+                username,
+                password,
+                rememberMe,
+                true
+            );
 
             await identitySecurityLogManager.SaveAsync(
                 new IdentitySecurityLogContext()
@@ -38,7 +49,7 @@ public class AccountController(
                 return Redirect("~/");
             }
 
-            return Redirect($"~/login?error=Login Failed:{result.GetResultAsString()}");
+            return RedirectWithError("~/login", result.GetResultAsString());
         }
         catch (Exception ex)
         {
@@ -47,15 +58,152 @@ public class AccountController(
     }
 
     [HttpPost("/account/externallogin")]
-    public async Task<IActionResult> ExternalLoginAsync(string provider, string returnUrl)
+    public async Task<IActionResult> ExternalLoginAsync(
+        string provider,
+        string returnUrl,
+        string returnUrlHash
+    )
     {
+        var redirectUrl = Url.Action(
+            "ExternalLoginCallback",
+            "Account",
+            new { returnUrl, returnUrlHash }
+        );
         var properties = signInManager.ConfigureExternalAuthenticationProperties(
             provider,
-            returnUrl
+            redirectUrl
         );
         properties.Items["scheme"] = provider;
 
         return await Task.FromResult(Challenge(properties, provider));
+    }
+
+    [HttpGet("/account/externallogin/callback")]
+    public async Task<IActionResult> ExternalLoginCallbackAsync(
+        string returnUrl = "",
+        string returnUrlHash = "",
+        string remoteError = ""
+    )
+    {
+        if (!remoteError.IsNullOrEmpty())
+        {
+            Logger.LogWarning("External login callback error: {remoteError}", remoteError);
+            return RedirectWithError("~/login", remoteError);
+        }
+
+        await identityOptions.SetAsync();
+
+        var loginInfo = await signInManager.GetExternalLoginInfoAsync();
+        if (loginInfo == null)
+        {
+            Logger.LogWarning("External login info is not available");
+            return RedirectWithError("~/login", "External login info is not available");
+        }
+
+        var result = await signInManager.ExternalLoginSignInAsync(
+            loginInfo.LoginProvider,
+            loginInfo.ProviderKey,
+            isPersistent: false,
+            bypassTwoFactor: true
+        );
+
+        if (!result.Succeeded)
+        {
+            await identitySecurityLogManager.SaveAsync(
+                new IdentitySecurityLogContext()
+                {
+                    Identity = IdentitySecurityLogIdentityConsts.IdentityExternal,
+                    Action = "Login" + result
+                }
+            );
+        }
+
+        if (result.IsLockedOut)
+        {
+            Logger.LogWarning("External login callback error: user is locked out!");
+            return RedirectWithError(
+                "~/login",
+                "External login callback error: user is locked out!"
+            );
+        }
+
+        if (result.IsNotAllowed)
+        {
+            Logger.LogWarning("External login callback error: user is not allowed!");
+            return RedirectWithError(
+                "~/login",
+                "External login callback error: user is not allowed!"
+            );
+        }
+
+        Volo.Abp.Identity.IdentityUser? user;
+        if (result.Succeeded)
+        {
+            user = await userManager.FindByLoginAsync(
+                loginInfo.LoginProvider,
+                loginInfo.ProviderKey
+            );
+            if (user != null)
+            {
+                await identityDynamicClaimsPrincipalContributorCache.ClearAsync(
+                    user.Id,
+                    user.TenantId
+                );
+            }
+
+            return await RedirectSafelyAsync(returnUrl, returnUrlHash);
+        }
+
+        var email =
+            loginInfo.Principal.FindFirstValue(AbpClaimTypes.Email)
+            ?? loginInfo.Principal.FindFirstValue(ClaimTypes.Email);
+        if (email.IsNullOrWhiteSpace())
+        {
+            return Redirect(
+                $"~/register?IsExternalLogin=true&ExternalLoginAuthSchema={loginInfo.LoginProvider}&ReturnUrl={returnUrl}"
+            );
+        }
+
+        user = await userManager.FindByEmailAsync(email);
+        if (user == null)
+        {
+            return Redirect(
+                $"~/register?IsExternalLogin=true&ExternalLoginAuthSchema={loginInfo.LoginProvider}&ReturnUrl={returnUrl}"
+            );
+        }
+
+        if (
+            await userManager.FindByLoginAsync(loginInfo.LoginProvider, loginInfo.ProviderKey)
+            == null
+        )
+        {
+            var identityResult = await userManager.AddLoginAsync(user, loginInfo);
+            if (!identityResult.Succeeded)
+            {
+                Logger.LogWarning("Add Login Error:{@Errors}", identityResult.Errors);
+                return RedirectWithError(
+                    "~/login",
+                    identityResult
+                        .Errors.Select(e => $"[{e.Code}] {e.Description}")
+                        .JoinAsString(", ")
+                );
+            }
+        }
+
+        await signInManager.SignInAsync(user, false);
+
+        await identitySecurityLogManager.SaveAsync(
+            new IdentitySecurityLogContext()
+            {
+                Identity = IdentitySecurityLogIdentityConsts.IdentityExternal,
+                Action = result.ToIdentitySecurityLogAction(),
+                UserName = user.Name
+            }
+        );
+
+        await identityDynamicClaimsPrincipalContributorCache.ClearAsync(user.Id, user.TenantId);
+
+        return await RedirectSafelyAsync(returnUrl, returnUrlHash);
     }
 
     [HttpGet("/account/logout")]
