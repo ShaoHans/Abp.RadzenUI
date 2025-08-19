72b2d75880b7aed8ad9872cf164b420db1e4a1c2
ShaoHans <shaomenghao@hotmail.com>
2025-08-19 21:34:15 +0800

    upgrade Radzen and fix bug

--- samples/CRM.EntityFrameworkCore/Migrations/20250819131901_Update_BackgroundJobsDbContext.Designer.cs
+++ samples/CRM.EntityFrameworkCore/Migrations/20250819131901_Update_BackgroundJobsDbContext.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CRM.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Volo.Abp.EntityFrameworkCore;
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace CRM.Migrations
 {
     [DbContext(typeof(CRMDbContext))]
-    partial class CRMDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250819131901_Update_BackgroundJobsDbContext")]
+    partial class Update_BackgroundJobsDbContext
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ samples/CRM.EntityFrameworkCore/Migrations/20250819131901_Update_BackgroundJobsDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRM.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_BackgroundJobsDbContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ApplicationName",
+                table: "AbpBackgroundJobs",
+                type: "character varying(96)",
+                maxLength: 96,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApplicationName",
+                table: "AbpBackgroundJobs");
+        }
+    }
+}
