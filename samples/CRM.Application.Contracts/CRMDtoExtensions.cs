ef2dd68c6176395cd314a890d4cce41a68b3f9ec
ShaoHans <shaomenghao@hotmail.com>
2025-12-26 23:13:39 +0800

    dynamic generate column

--- samples/CRM.Application.Contracts/CRMDtoExtensions.cs
+++ samples/CRM.Application.Contracts/CRMDtoExtensions.cs
@@ -14,67 +14,67 @@ public static class CRMDtoExtensions
     {
         OneTimeRunner.Run(() =>
         {
-            ObjectExtensionManager.Instance.AddOrUpdateProperty<IdentityUserCreateDto, string>(
-                "HireDate",
-                propertyInfo =>
-                {
-                    propertyInfo.Attributes.Add(new RequiredAttribute());
-                    propertyInfo.Attributes.Add(new DataTypeAttribute(DataType.Date));
-                    propertyInfo.CheckPairDefinitionOnMapping = false;
-                }
-            );
+            //ObjectExtensionManager.Instance.AddOrUpdateProperty<IdentityUserCreateDto, string>(
+            //    "HireDate",
+            //    propertyInfo =>
+            //    {
+            //        propertyInfo.Attributes.Add(new RequiredAttribute());
+            //        propertyInfo.Attributes.Add(new DataTypeAttribute(DataType.Date));
+            //        propertyInfo.CheckPairDefinitionOnMapping = false;
+            //    }
+            //);
 
-            ObjectExtensionManager.Instance.AddOrUpdateProperty<IdentityUserUpdateDto, string>(
-                "HireDate",
-                propertyInfo =>
-                {
-                    propertyInfo.Attributes.Add(new RequiredAttribute());
-                    propertyInfo.Attributes.Add(new DataTypeAttribute(DataType.Date));
-                    propertyInfo.CheckPairDefinitionOnMapping = false;
-                }
-            );
+            //ObjectExtensionManager.Instance.AddOrUpdateProperty<IdentityUserUpdateDto, string>(
+            //    "HireDate",
+            //    propertyInfo =>
+            //    {
+            //        propertyInfo.Attributes.Add(new RequiredAttribute());
+            //        propertyInfo.Attributes.Add(new DataTypeAttribute(DataType.Date));
+            //        propertyInfo.CheckPairDefinitionOnMapping = false;
+            //    }
+            //);
 
-            ObjectExtensionManager.Instance.AddOrUpdateProperty<IdentityUserDto, string>(
-                "HireDate",
-                propertyInfo =>
-                {
-                    propertyInfo.Configuration.Add("Width", "150px");
-                    propertyInfo.Configuration.Add("LocalizationKey", "DisplayName:HireDate");
-                    //propertyInfo.Configuration.Add("Title", "DisplayName");
-                    propertyInfo.Configuration.Add("FormatString", "{0:yyyy-MM-dd}");
-                    propertyInfo.CheckPairDefinitionOnMapping = false;
-                }
-            );
+            //ObjectExtensionManager.Instance.AddOrUpdateProperty<IdentityUserDto, string>(
+            //    "HireDate",
+            //    propertyInfo =>
+            //    {
+            //        propertyInfo.Configuration.Add("Width", "150px");
+            //        propertyInfo.Configuration.Add("LocalizationKey", "DisplayName:HireDate");
+            //        //propertyInfo.Configuration.Add("Title", "DisplayName");
+            //        propertyInfo.Configuration.Add("FormatString", "{0:yyyy-MM-dd}");
+            //        propertyInfo.CheckPairDefinitionOnMapping = false;
+            //    }
+            //);
 
-            ObjectExtensionManager.Instance.AddOrUpdateProperty<TenantCreateDto, string>(
-                "TenantDisplayName",
-                propertyInfo =>
-                {
-                    propertyInfo.Attributes.Add(new DataTypeAttribute(DataType.Text));
-                    propertyInfo.CheckPairDefinitionOnMapping = false;
-                }
-            );
+            //ObjectExtensionManager.Instance.AddOrUpdateProperty<TenantCreateDto, string>(
+            //    "TenantDisplayName",
+            //    propertyInfo =>
+            //    {
+            //        propertyInfo.Attributes.Add(new DataTypeAttribute(DataType.Text));
+            //        propertyInfo.CheckPairDefinitionOnMapping = false;
+            //    }
+            //);
 
-            ObjectExtensionManager.Instance.AddOrUpdateProperty<TenantUpdateDto, string>(
-                "TenantDisplayName",
-                propertyInfo =>
-                {
-                    propertyInfo.Attributes.Add(new DataTypeAttribute(DataType.Text));
-                    propertyInfo.CheckPairDefinitionOnMapping = false;
-                }
-            );
+            //ObjectExtensionManager.Instance.AddOrUpdateProperty<TenantUpdateDto, string>(
+            //    "TenantDisplayName",
+            //    propertyInfo =>
+            //    {
+            //        propertyInfo.Attributes.Add(new DataTypeAttribute(DataType.Text));
+            //        propertyInfo.CheckPairDefinitionOnMapping = false;
+            //    }
+            //);
 
-            ObjectExtensionManager.Instance.AddOrUpdateProperty<TenantDto, string>(
-                "TenantDisplayName",
-                propertyInfo =>
-                {
-                    propertyInfo.Configuration.Add("Width", "200px");
-                    propertyInfo.Configuration.Add("LocalizationKey", "DisplayName:TenantDisplayName");
-                    //propertyInfo.Configuration.Add("Title", "DisplayName");
-                    propertyInfo.Configuration.Add("FormatString", "");
-                    propertyInfo.CheckPairDefinitionOnMapping = false;
-                }
-            );
+            //ObjectExtensionManager.Instance.AddOrUpdateProperty<TenantDto, string>(
+            //    "TenantDisplayName",
+            //    propertyInfo =>
+            //    {
+            //        propertyInfo.Configuration.Add("Width", "200px");
+            //        propertyInfo.Configuration.Add("LocalizationKey", "DisplayName:TenantDisplayName");
+            //        //propertyInfo.Configuration.Add("Title", "DisplayName");
+            //        propertyInfo.Configuration.Add("FormatString", "");
+            //        propertyInfo.CheckPairDefinitionOnMapping = false;
+            //    }
+            //);
         });
     }
 }
