e1d556768d364f994985cd3f1b5c8ba51114eee3
ShaoHans <shaomenghao@hotmail.com>
2025-12-28 21:10:19 +0800

    custome UI Place Holder

--- samples/CRM.Domain/CRMConsts.cs
+++ samples/CRM.Domain/CRMConsts.cs
@@ -5,5 +5,5 @@ public static class CRMConsts
     public const string DbTablePrefix = "App";
     public const string? DbSchema = null;
     public const string AdminEmailDefaultValue = "admin@crm.com";
-    public const string AdminPasswordDefaultValue = "2024@09$14&Start";
+    public const string AdminPasswordDefaultValue = "2024@09$14&Start";    
 }
