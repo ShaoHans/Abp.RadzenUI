71d61d54af7387766ee9eb1b2db02fec2c5e819e
ShaoHans <shaomenghao@hotmail.com>
2026-04-01 21:27:58 +0800

    upgrade abp 10.2.0 and radzen

---
+++ samples/CRM.EntityFrameworkCore/Migrations/20260401132253_UpdateAbp10.2.0.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRM.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateAbp1020 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Leaved",
+                table: "AbpUsers",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PropertyTypeFullName",
+                table: "AbpEntityPropertyChanges",
+                type: "character varying(512)",
+                maxLength: 512,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(64)",
+                oldMaxLength: 64);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EntityTypeFullName",
+                table: "AbpEntityChanges",
+                type: "character varying(512)",
+                maxLength: 512,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(128)",
+                oldMaxLength: 128);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Leaved",
+                table: "AbpUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PropertyTypeFullName",
+                table: "AbpEntityPropertyChanges",
+                type: "character varying(64)",
+                maxLength: 64,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(512)",
+                oldMaxLength: 512);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EntityTypeFullName",
+                table: "AbpEntityChanges",
+                type: "character varying(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(512)",
+                oldMaxLength: 512);
+        }
+    }
+}
