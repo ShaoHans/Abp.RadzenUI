fb11688e752ff877caecff1d5d51d041b01c9355
ShaoHans <shaomenghao@hotmail.com>
2026-04-23 23:06:03 +0800

    Prevent duplicate submission

--- src/Abp.Blazor.Server.RadzenUI/Models/DialogFromOption.cs
+++ src/Abp.Blazor.Server.RadzenUI/Models/DialogFromOption.cs
@@ -7,4 +7,35 @@ public class DialogFromOption<TCreateOrUpdateInput> where TCreateOrUpdateInput :
     public Func<TCreateOrUpdateInput, Task> OnSubmit { get; set; } = default!;
 
     public Action OnCancel { get; set; } = default!;
+
+    public bool IsSaving { get; private set; }
+
+    public async Task SubmitAsync(
+        TCreateOrUpdateInput model,
+        Func<Task>? stateHasChangedAsync = null
+    )
+    {
+        if (IsSaving)
+        {
+            return;
+        }
+
+        IsSaving = true;
+        await NotifyStateChangedAsync(stateHasChangedAsync);
+
+        try
+        {
+            await OnSubmit(model);
+        }
+        finally
+        {
+            IsSaving = false;
+            await NotifyStateChangedAsync(stateHasChangedAsync);
+        }
+    }
+
+    static Task NotifyStateChangedAsync(Func<Task>? stateHasChangedAsync)
+    {
+        return stateHasChangedAsync is null ? Task.CompletedTask : stateHasChangedAsync();
+    }
 }
